79625b4faefe48356edd106170a345b31d4b13d0
dazizo25 <dazizo25@gmail.com>
2024-01-30 02:04:17 +0000

    updating my project

--- app/src/main/java/com/example/dma_t2p/MainActivity.java
+++ app/src/main/java/com/example/dma_t2p/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // Handle system insets
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Set up RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final TaskListAdapter adapter = new TaskListAdapter(new TaskListAdapter.TaskDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
